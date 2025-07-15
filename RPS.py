e41e2dff6c0d1fa43664d5f7a83e4a8680bbbe83
MrSpecks <KagisoMfusi28@gmail.com>
2025-07-15 20:14:47 +0000

    Won this challenege

--- RPS.py
+++ RPS.py
@@ -1,10 +1,60 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+my_history = []
+opp_history = []
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+def player(prev_opponent_play):
+    if prev_opponent_play != '':
+        opp_history.append(prev_opponent_play)
+    moves = ['R', 'P', 'S']
+    beats = {'R': 'P', 'P': 'S', 'S': 'R'}
+    loses_to = {v: k for k, v in beats.items()}
 
-    return guess
+    # Start with random if no history
+    if not my_history:
+        move = random.choice(moves)
+        my_history.append(move)
+        return move
+
+    # Detect if opponent copies last move:
+    if len(opp_history) > 1:
+        if opp_history[-1] == my_history[-1]:
+            # Opponent copied you last round, so play what beats your last move
+            move = beats[my_history[-1]]
+            my_history.append(move)
+            return move
+
+    # Detect if opponent plays what beats your last move:
+    if len(opp_history) > 1:
+        if opp_history[-1] == beats[my_history[-1]]:
+            # Opponent tries to beat your last move, so play what beats their last move
+            move = beats[opp_history[-1]]
+            my_history.append(move)
+            return move
+
+    # Check for losing streak (last 3 rounds)
+    if len(my_history) >= 3:
+        losses = 0
+        for i in range(-3, 0):
+            if opp_history[i] == beats[my_history[i]]:
+                losses += 1
+        if losses >= 2:
+            # Losing streak, switch to random to confuse opponent
+            move = random.choice(moves)
+            my_history.append(move)
+            return move
+
+    # Frequency analysis of opponent moves:
+    freq = {'R': 0, 'P': 0, 'S': 0}
+    for m in opp_history[-10:]:  # last 10 moves only
+        freq[m] += 1
+
+    prediction = max(freq, key=freq.get)
+    # With 10% chance, randomize to avoid being predictable
+    if random.random() < 0.1:
+        move = random.choice(moves)
+    else:
+        move = beats[prediction]
+
+    my_history.append(move)
+    return move
